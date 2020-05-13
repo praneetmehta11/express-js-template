ea6f776bdf75896503027992be1210a9cea38456
Praneet Mehta <praneetmehta11@gmail.com>
2020-05-13 17:07:14 +0530

    Index page modified

--- routes/index.router.js
+++ routes/index.router.js
@@ -6,8 +6,5 @@ router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './../views/index.html'))
 })
 
-router.get('/test', function(req, res) {
-    res.sendFile(path.join(__dirname, './../views/test.html'))
-})
 
 module.exports = router
